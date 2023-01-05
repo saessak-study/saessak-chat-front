7054de95b5153cf4fa24b5e7df62ae35f169f50d
SungBoShim <90492088+Shim-sim@users.noreply.github.com>
2023-01-05 18:15:26 +0900

    유효성 검사 추가 및 UI 일부수정 (#15)
    * refactor: 기존 공통 컴포넌트 FindUserInfo로 경로 수정 및 네이밍 변경
    
    * 이 부분***
    제거 prettier 때문에 따옴표 변경됐습니당...
    이거 저희 공통으로 맞춰봐요
    
    * refactor: 아이디,비밀번호 찾기 버튼 클릭 에러 수정했습니다.
    
    * refactor: 아이디 찾기, 비밀번호 찾기 css재사용위해서 파일 name 변경
    
    * feat: 비밀번호 찾기 컴포넌트 구현
    
    * refactor: 주석 제거 및 props로 받는 부분 하드코딩으로 수정.
    
    * style: 간격 추가했습니다.
    
    * feat: 아이디, 비밀번호 찾기 유효성 검사 추가
    
    1.line.30 함수는 화면 전환 시 기존 input초기화 시킴
    2. 유효성 검사 함수 하나로 묶고싶지만.. 역량 부족 코드 리팩토링 필요
    
    * style: 스타일링 일부 수정

---
+++ src/components/FindUserInfo/FindIdInput.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import styles from '../FindUserInfo/css/FindIdPwInput.module.css';
+import Modal from './Modal';
+
+/**
+ * TODO: Modal 수정 및 보완 필요(데이터 바인딩)
+ */
+const FindIdInput = ({
+  modal,
+  onFindIdHandler,
+  modalHandler,
+  onChange,
+  nameEmpty,
+  emailEmpty,
+  emailValid,
+}) => {
+  return (
+    <>
+      <div className={styles.input_container}>
+        <div className={styles.input_form_box}>
+          <div className={styles.input_info}>
+            <label>
+              이름
+              <input onChange={onChange} name="checkName" />
+            </label>
+            <label>
+              이메일
+              <input onChange={onChange} name="checkEmail" />
+            </label>
+          </div>
+          <div className={styles.check_btn_container}>
+            <button onClick={onFindIdHandler}>확인</button>
+          </div>
+        </div>
+        <Modal modalHandler={modalHandler} modal={modal} />
+      </div>
+      <div className={styles.check_valid_box}>
+        <span className={styles.check_valid_message}>
+          {nameEmpty && '※ 이름을 입력해주세요 ※'}
+          {emailEmpty && '※ 이메일을 입력해주세요 ※ '}
+          {emailValid && '※ 이메일 형식이 맞지 않습니다 ※'}
+        </span>
+      </div>
+    </>
+  );
+};
+
+export default FindIdInput;
