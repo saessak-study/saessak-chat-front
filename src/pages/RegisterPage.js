95af0a7ccf94ac6a76548bcb890635676b5f8829
ShimSungBo <shimdb96@gmail.com>
2023-01-05 09:02:37 +0900

    이 부분*** 제거 prettier 때문에 따옴표 변경됐습니당... 이거 저희 공통으로 맞춰봐요

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -17,28 +17,26 @@ const RegisterPage = () => {
         <div className={styles.register_warningMSG}>
           4~12자의 영문 대소문자와 숫자로만 입력해주세요.
         </div>
-        <RegisterInput message={"8~16자의 소문자,숫자,특수문자를 사용하세요." } />
-        <RegisterInput message={"비밀번호가 일치하지 않습니다." } />
-        <RegisterInput message={"올바른 정보를 입력해주세요." } />
-        <RegisterInput message={"이메일 형식이 맞지 않습니다." } />
-        <div className={styles.register_btn}>
-          회원가입
-        </div>
-        <Link to='/' style={{ textDecoration: 'none' }}>
-          <div className={styles.login_routeBtn}>
-            로그인 화면으로 돌아가기
-          </div>
+        <RegisterInput
+          message={'8~16자의 소문자,숫자,특수문자를 사용하세요.'}
+        />
+        <RegisterInput message={'비밀번호가 일치하지 않습니다.'} />
+        <RegisterInput message={'올바른 정보를 입력해주세요.'} />
+        <RegisterInput message={'이메일 형식이 맞지 않습니다.'} />
+        <div className={styles.register_btn}>회원가입</div>
+        <Link to="/" style={{ textDecoration: 'none' }}>
+          <div className={styles.login_routeBtn}>로그인 화면으로 돌아가기</div>
         </Link>
         <div className={styles.idchk_complete}>
           이 아이디는 사용가능한 아이디입니다.
-            <div className={styles.idButtonBox}>
-              <div className={styles.usingID}>아이디 사용</div>
-              <div className={styles.cancelID}>취소</div>
-            </div>
+          <div className={styles.idButtonBox}>
+            <div className={styles.usingID}>아이디 사용</div>
+            <div className={styles.cancelID}>취소</div>
+          </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default RegisterPage;
