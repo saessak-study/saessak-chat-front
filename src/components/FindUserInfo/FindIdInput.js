9fda8baca6103f682e57ead847269a478c872d66
ShimSungBo <shimdb96@gmail.com>
2023-01-05 16:26:51 +0900

    feat: 아이디, 비밀번호 찾기 유효성 검사 추가
    1.line.30 함수는 화면 전환 시 기존 input초기화 시킴
    2. 유효성 검사 함수 하나로 묶고싶지만.. 역량 부족 코드 리팩토링 필요

--- src/components/FindUserInfo/FindIdInput.js
+++ src/components/FindUserInfo/FindIdInput.js
@@ -5,26 +5,43 @@ import Modal from './Modal';
 /**
  * TODO: Modal 수정 및 보완 필요(데이터 바인딩)
  */
-const FindIdInput = ({ modal, modalHandler }) => {
+const FindIdInput = ({
+  modal,
+  onFindIdHandler,
+  modalHandler,
+  onChange,
+  nameEmpty,
+  emailEmpty,
+  emailValid,
+}) => {
   return (
-    <div className={styles.input_container}>
-      <div className={styles.input_form_box}>
-        <div className={styles.input_info}>
-          <label>
-            이름
-            <input />
-          </label>
-          <label>
-            이메일
-            <input />
-          </label>
+    <>
+      <div className={styles.input_container}>
+        <div className={styles.input_form_box}>
+          <div className={styles.input_info}>
+            <label>
+              이름
+              <input onChange={onChange} name="checkName" />
+            </label>
+            <label>
+              이메일
+              <input onChange={onChange} name="checkEmail" />
+            </label>
+          </div>
+          <div className={styles.check_btn_container}>
+            <button onClick={onFindIdHandler}>확인</button>
+          </div>
         </div>
-        <div className={styles.check_btn_container}>
-          <button onClick={modalHandler}>확인</button>
-        </div>
+        <Modal modalHandler={modalHandler} modal={modal} />
       </div>
-      <Modal modalHandler={modalHandler} modal={modal} />
-    </div>
+      <div className={styles.check_valid_box}>
+        <span className={styles.check_valid_message}>
+          {nameEmpty && '※ 이름을 입력해주세요 ※'}
+          {emailEmpty && '※ 이메일을 입력해주세요 ※ '}
+          {emailValid && '※ 이메일 형식이 맞지 않습니다 ※'}
+        </span>
+      </div>
+    </>
   );
 };
 
--- src/pages/FindUserInfoPage.js
+++ src/pages/FindUserInfoPage.js
@@ -5,15 +5,128 @@ import FindPwInput from '../components/FindUserInfo/FindPwInput';
 import styles from '../style/css/findUserInfoPage.module.css';
 
 const FindUserInfoPage = () => {
-  const [toggle, setToggle] = useState(true);
+  const [inputs, setInput] = useState({
+    checkId: '',
+    checkName: '',
+    checkEmail: '',
+  });
+  const { checkId, checkName, checkEmail } = inputs;
+  const [idEmpty, setIdEmpty] = useState(false);
+  const [nameEmpty, setNameEmpty] = useState(false);
+  const [emailEmpty, setEmailEmpty] = useState(false);
+  const [emailValid, setEmailValid] = useState(false);
+  const [idPwToggle, setIdPwToggle] = useState(true);
   const [modal, setModal] = useState(false);
 
-  const toggleHandler = (e) => {
-    setToggle(!toggle);
+  const onChange = (e) => {
+    const { value, name } = e.target;
+    setInput({
+      ...inputs,
+      [name]: value,
+    });
   };
 
-  const modalHandler = (e) => {
-    setModal(!modal);
+  /** 화면 전환 및 전환 시 기존데이터 초기화 */
+  const IdAndPwToggleHandler = () => {
+    setIdPwToggle(!idPwToggle);
+    setEmailValid(false);
+    setNameEmpty(false);
+    setEmailEmpty(false);
+  };
+
+  const onFindIdHandler = (e) => {
+    const exp = /^([0-9a-zA-Z_\.-]+)@([0-9a-zA-Z_-]+)(\.[0-9a-zA-Z_-]+){1,2}$/;
+
+    e.preventDefault();
+    if (!checkName && !checkEmail) {
+      setNameEmpty(true);
+      setEmailEmpty(false);
+      setEmailValid(false);
+      return;
+    }
+
+    if (!checkEmail) {
+      setEmailEmpty(true);
+      setNameEmpty(false);
+      setEmailValid(false);
+      return;
+    }
+
+    if (!checkName) {
+      setNameEmpty(true);
+      setEmailEmpty(false);
+      setEmailValid(false);
+      return;
+    }
+
+    if (checkName && checkEmail) {
+      if (exp.test(checkEmail)) {
+        setModal(true);
+        setEmailEmpty(false);
+        setNameEmpty(false);
+        setEmailValid(false);
+      } else {
+        setEmailValid(true);
+        setEmailEmpty(false);
+        setNameEmpty(false);
+      }
+    }
+  };
+
+  const onFindPwHandler = (e) => {
+    const exp = /^([0-9a-zA-Z_\.-]+)@([0-9a-zA-Z_-]+)(\.[0-9a-zA-Z_-]+){1,2}$/;
+
+    e.preventDefault();
+    if (!checkName && !checkEmail && !checkId) {
+      setNameEmpty(true);
+      setEmailEmpty(false);
+      setEmailValid(false);
+      setIdEmpty(false);
+      return;
+    }
+
+    if (!checkEmail) {
+      setEmailEmpty(true);
+      setNameEmpty(false);
+      setEmailValid(false);
+      setIdEmpty(false);
+      return;
+    }
+
+    if (!checkName) {
+      setNameEmpty(true);
+      setEmailEmpty(false);
+      setEmailValid(false);
+      setIdEmpty(false);
+      return;
+    }
+
+    if (!checkId) {
+      setIdEmpty(true);
+      setEmailEmpty(false);
+      setEmailValid(false);
+      setNameEmpty(false);
+      return;
+    }
+
+    if (checkName && checkEmail && checkId) {
+      if (exp.test(checkEmail)) {
+        setModal(true);
+        setEmailEmpty(false);
+        setNameEmpty(false);
+        setEmailValid(false);
+        setIdEmpty(false);
+      } else {
+        setEmailValid(true);
+        setEmailEmpty(false);
+        setNameEmpty(false);
+        setIdEmpty(false);
+      }
+    }
+  };
+
+  const modalHandler = () => {
+    setModal(false);
   };
 
   return (
@@ -21,23 +134,49 @@ const FindUserInfoPage = () => {
       <div className={styles.app_name}>🌱SaessakChat🌱</div>
       <div className={styles.findInfo_box}>
         <div
-          className={`${toggle ? styles.findId : styles.findIdAndPw_toggle}`}
+          className={`${
+            idPwToggle ? styles.findId : styles.findIdAndPw_toggle
+          }`}
         >
-          <span onClick={toggle ? null : toggleHandler}>아이디 찾기</span>
+          <span onClick={idPwToggle ? null : IdAndPwToggleHandler}>
+            아이디 찾기
+          </span>
         </div>
         <div
-          className={`${toggle ? styles.findIdAndPw_toggle : styles.findPw}`}
+          className={`${
+            idPwToggle ? styles.findIdAndPw_toggle : styles.findPw
+          }`}
         >
-          <span onClick={toggle ? toggleHandler : null}>비밀번호 찾기</span>
+          <span onClick={idPwToggle ? IdAndPwToggleHandler : null}>
+            비밀번호 찾기
+          </span>
         </div>
       </div>
       <div className={styles.findIdAndPw_container}>
-        {toggle ? (
-          <FindIdInput modal={modal} modalHandler={modalHandler} />
+        {idPwToggle ? (
+          <FindIdInput
+            modal={modal}
+            onFindIdHandler={onFindIdHandler}
+            onChange={onChange}
+            modalHandler={modalHandler}
+            nameEmpty={nameEmpty}
+            emailEmpty={emailEmpty}
+            emailValid={emailValid}
+          />
         ) : (
-          <FindPwInput modal={modal} modalHandler={modalHandler} />
+          <FindPwInput
+            modal={modal}
+            onFindPwHandler={onFindPwHandler}
+            onChange={onChange}
+            modalHandler={modalHandler}
+            nameEmpty={nameEmpty}
+            emailEmpty={emailEmpty}
+            emailValid={emailValid}
+            idEmpty={idEmpty}
+          />
         )}
       </div>
+
       <Link to="/" style={{ textDecoration: 'none' }}>
         <div className={styles.navigate_text}>로그인 페이지로 돌아가기</div>
       </Link>
