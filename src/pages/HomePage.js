a77ddd15090a9a76e282364c5f6b1c168259cf37
DoggySummer <91588779+DoggySummer@users.noreply.github.com>
2023-01-06 14:42:56 +0900

    feat 좌측 컨테이너 완성

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,47 @@
 import React from 'react';
+import styles from '../style/css/homePage.module.css';
 
 const HomePage = () => {
   return (
-    <div>
-
+    <div className={styles.mainPage}>
+      <div className={styles.left_container}>
+        <div className={styles.main_logo}>🌱SaessakChat🌱</div>
+        <div className={styles.user_container}>
+          <div className={styles.user_online}>🙌현재 접속중인 유저</div>
+          <div className={styles.user_status_container}>
+            <div className={styles.user_status}>
+              <div>유저1 🟢</div>
+            </div>
+            <div className={styles.user_status}>
+              <div>유저2 ⚫</div>
+            </div>
+            <div className={styles.user_status}>
+              <div>유저1 🟢</div>
+            </div>
+            <div className={styles.user_status}>
+              <div>유저2 ⚫</div>
+            </div>
+            <div className={styles.user_status}>
+              <div>유저1 🟢</div>
+            </div>
+            <div className={styles.user_status}>
+              <div>유저2 ⚫</div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.user_logout}>🚨로그아웃하기</div>
+      </div>
+      <div className={styles.chatlog_container}>
+        <div className={styles.typed_chat_fromMe}>
+          <div className={styles.typed_chat_info}>
+            <div className={styles.typed_chat_name}>정길웅</div>
+            <div className={styles.typed_chat_time}>2023.01.06 (금) 오전 8시 40분</div>
+          </div>
+          <div className={styles.typed_chat_message}>
+            안녕하세요! 혹시 고쳐야 할 UI나 추가하면 좋을 것 같은 기능도 말씀해주세요!
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
