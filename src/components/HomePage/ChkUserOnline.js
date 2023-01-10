369fdb36794936c47eb88cc444ac57c946b0be26
JUNG GIL WOONG <91588779+DoggySummer@users.noreply.github.com>
2023-01-10 17:05:46 +0900

    UI design (#18)
    * feat 좌측 컨테이너 완성
    
    * feat 오른쪽 컨테이너 완성직전
    
    * feat 디자인은 다 끝냄
    
    * feat json파일 chatlog로 부르기 성공
    
    * placeholder 추가
    
    * input함수 넣기
    
    * 회원가입 alert까지 만들었음
    
    * 비밀번호 일치 확인 함수

---
+++ src/components/HomePage/ChkUserOnline.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import styles from '../../style/css/homePage.module.css'
+
+const ChkUserOnline = ({
+  userName,
+  userOnline
+}) => {
+  return (
+    <div>
+      <div className={styles.user_status}>
+        <div> { userName } </div>
+        {
+          userOnline==true 
+          ? <div className={styles.user_OnlineCircle}></div> 
+          : <div className={styles.user_OfflineCircle}></div>
+        }
+      </div>
+    </div>
+  )
+}
+export default ChkUserOnline;
