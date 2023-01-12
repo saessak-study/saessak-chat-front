ec6d0a793b7b74737520d2764a88501d76fb0a2c
ShimSungBo <shimdb96@gmail.com>
2023-01-12 17:08:39 +0900

    refactor: 정규표현식 변수명 수정

--- src/constants/regEx.js
+++ src/constants/regEx.js
@@ -1,7 +1,9 @@
-export const rEmail =
-  /^([0-9a-zA-Z_\.-]+)@([0-9a-zA-Z_-]+)(\.[0-9a-zA-Z_-]+){1,2}$/;
+export const regEmail =
+  /^([0-9a-zA-Z_-]+)@([0-9a-zA-Z_-]+)(\.[0-9a-zA-Z_-]+){1,2}$/;
 
-export const rPassword =
+export const regPassword =
   /^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,16}$/;
 
-export const rId = /^[a-zA-z0-9]{4,12}$/;
+export const regId = /^[a-zA-z0-9]{4,12}$/;
+
+export const regName = /^[가-힣]+$/;
